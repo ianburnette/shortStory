02b8d1da2cb32c94e2d2256c5a431071c144da23
Ian Burnette <ian_burnette@yahoo.com>
2015-03-16 16:52:18 -0400

    character rigged and car ai
    character rigged and stands/looks at you (kinda)
    cars chase player

---
+++ Assets/carAITest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carAITest : MonoBehaviour {
+
+	public Transform destination;
+	NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.destination = destination.position;
+	}
+}
