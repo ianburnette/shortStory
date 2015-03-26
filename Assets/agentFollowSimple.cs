c83e9d1a02af8c9046f9d4d9614be7db949d4cb4
ianburnette <ian_burnette@yahoo.com>
2015-03-25 23:26:38 -0400

    more car work

---
+++ Assets/agentFollowSimple.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class agentFollowSimple : MonoBehaviour {
+
+	public Transform destination;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = destination.position;
+	}
+}
